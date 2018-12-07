968d509266dadab966f478ec0a7d948143e70225
weiqixu <weiqixuzj@163.com>
2018-12-07 12:00:31 +0800

    构建新项目

---
+++ resources/lang/ch/app.php
@@ -0,0 +1,75 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Label language files for All Module
+    |--------------------------------------------------------------------------
+    |
+     */
+
+    'name'           => '虚伪旗',
+    'name.html'      => '<b>虚伪</b>旗',
+    'name.short'     => '<b>L</b>l',
+    'admin.panel'    => '管理面板',
+    'dashboard'      => '仪表盘',
+    'rights'         => '&copy; 2018 All Rights Reserved, Built with <a href="https://laravel.com" target="_blank"><i class="fab fa-laravel"></i></a>, Created by <a href="http://renfos.com/" target="_blank">Renfos</a>.',
+    'version'        => '<b>版本</b> 开发',
+
+    'add'            => '添加',
+    'addnew'         => '新增',
+    'actions'        => '操作',
+    'approve'        => '批准',
+    'archive'        => '存档',
+    'unarchive'      => '未封存',
+    'back'           => '背部',
+    'cancel'         => '取消',
+    'close'          => '关闭',
+    'copy'           => '复制',
+    'complete'       => '已完成',
+    'create'         => '创建',
+    'dashboard'      => '仪表盘1111111',
+    'delete'         => '删除',
+    'draft'          => '草案',
+    'details'        => '详情',
+    'edit'           => '编辑',
+    'enterkeyword'   => '请输入要搜索的关键字',
+    'go'             => 'Go',
+    'help'           => 'Help',
+    'home'           => '目录',
+    'list'           => '列表',
+    'logout'         => '退出',
+    'logs'           => '日志',
+    'manage'         => '管理',
+    'more'           => '更多',
+    'new'            => 'New',
+    'no'             => 'No',
+    'opt'            => '选项',
+    'option'         => '选项',
+    'options'        => '多选项',
+    'order'          => 'Order',
+    'password'       => '密码',
+    'profile'        => '概述',
+    'publish'        => '发布时间',
+    'request'        => '请求',
+    'reports'        => '报告',
+    'reset'          => '还原',
+    'save'           => '保存',
+    'search'         => '搜索',
+    'settings'       => '设置',
+    'show'           => '展示',
+    'status'         => '状态',
+    'update'         => '更新',
+    'update_profile' => '更新配置文件',
+    'unpublish'      => '未公布',
+    'view'           => '视图',
+    'verify'         => '验证',
+    'yes'            => 'Yes',
+
+    'flash.success'  => 'Success',
+    'flash.error'    => 'Error',
+    'flash.warning'  => 'Warning',
+    'flash.info'     => 'Info',
+
+];
