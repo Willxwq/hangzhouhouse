dee19f9ef6ff69efeabf9160e40d19dd16d43969
weiqixu <weiqixuzj@163.com>
2019-05-31 10:26:43 +0800

    增加切换城市

--- resources/views/RealEstate/sellUpsAndDowns.blade.php
+++ resources/views/RealEstate/sellUpsAndDowns.blade.php
@@ -64,5 +64,5 @@
         });
 
     </script>
-    <script type="text/javascript" src="{{ URL::asset('/realEstate/js/sellUpsAndDowns.js?1') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('/realEstate/js/sellUpsAndDowns.js?111') }}"></script>
 @endsection
