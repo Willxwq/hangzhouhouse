d2e7511bdb206ae0266fa20d2f19f86f3d28c084
weiqixu <weiqixuzj@163.com>
2019-06-04 16:21:43 +0800

    增加中位数，成交量，平均价

--- resources/views/RealEstate/priceRiseAndDecline.blade.php
+++ resources/views/RealEstate/priceRiseAndDecline.blade.php
@@ -67,6 +67,14 @@
 
     <script>
         $(document).ready(function () {
+            function getUrlParam(name) {
+                var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+                var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+                if (r != null) return encodeURI(r[2]); return null; //返回参数值
+            }
+
+            $("#city").val(getUrlParam('city'));
+
             $('#city').change(function(){
                 document.location.href = "/sell/priceRiseAndDecline?city=" + $('#city').val();
             });
