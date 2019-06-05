e22e0d9c2cdcff7132e5e10ea089cdeab30ca2fc
weiqixu <weiqixuzj@163.com>
2019-06-05 09:13:19 +0800

    增加中位数，成交量，平均价

--- resources/views/layouts/_menu.blade.php
+++ resources/views/layouts/_menu.blade.php
@@ -2,6 +2,6 @@
 <ul>
     <li><a href="/community">小区</a></li>
     <li><a href="/sell/sellUpsAndDowns">挂牌与成交榜单</a></li>
-    <li><a href="/sell/priceRiseAndDecline">同比涨跌幅</a></li>
+    <li><a href="/sell/priceRiseAndDecline?city=0">同比涨跌幅</a></li>
 </ul>
 {{--<div class="meny-arrow"></div>--}}
