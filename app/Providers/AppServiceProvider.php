8e39130a29b634edeb5e541a961b6ba5aa591497
weiqixu <weiqixuzj@163.com>
2019-10-18 10:28:34 +0800

    保存

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        error_reporting(E_ALL);
+        error_reporting(E_ALL ^ E_NOTICE);
     }
 
     /**
